58196e4fb4c0daccc9b46a48b3f56c4e86819110
HaderMostafa <hadeermostafa.094@gmail.com>
2022-04-06 03:34:32 +0200

    jqueryTask

---
+++ items.js
@@ -0,0 +1,23 @@
+[
+	{
+		"id": "1",
+		"name": "Charbroiled BBQ",
+		"price": 120,
+		"description": "Grilled burger topped with sweet onion, BBQ sauce, creamy Charbroiled sauce and Swiss cheese",
+		"image": "images/1.jpg"
+	},
+	{
+		"id": "2",
+		"name": "Hitchhiker",
+		"price": 150,
+		"description": "Crispy mini mozzarella sticks, loaded with ketchup and mustard drops, beef bacon and creamy",
+		"image": "images/2.jpg"
+	},
+	{
+		"id": "3",
+		"name": "Old School",
+		"price": 100,
+		"description": "Pure beef burger patty, topped with our signature Buffalo sauce and cheddar cheese",
+		"image": "images/3.jpg"
+	}
+]
